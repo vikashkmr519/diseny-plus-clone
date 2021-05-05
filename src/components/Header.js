a5330e2cdfe4c214949b832bb121247edbd5c69b
Vikash Kumar <vikashkmr519@gmail.com>
2021-05-06 00:02:06 +0530

    ADD:header

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,6 +36,7 @@ function Header() {
           <span>SERIES</span>
         </a>
       </NavMenu>
+      <UserImg src="/images/profile-image.jpg" />
     </Nav>
   );
 }
@@ -56,11 +57,15 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
   display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
 
   a {
     display: flex;
     align-items: center;
     padding: 0 12px;
+    cursor: pointer;
 
     img {
       height: 20px;
@@ -69,6 +74,34 @@ const NavMenu = styled.div`
     span {
       font-size: 13px;
       letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
     }
   }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
